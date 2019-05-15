bc72fa2c0edd67c75bff356309e9e6eb1acbc702
mathematical-algorithm-3478 <jlboyer1@gmail.com>
2019-05-15 22:34:42 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move(board, index)
-  index.to_i
-  if index > 0 && index < 10 && index.is_a? Integer && !position_taken?(board,index)
+  index = index.to_i
+  if index > 0 && index < 10 && !position_taken?(board,index)
     true
   else
     false
